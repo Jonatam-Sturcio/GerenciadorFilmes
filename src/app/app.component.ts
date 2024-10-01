b35a25342f7af7dcf1042f845767f31c7ed2bc6c
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-10-01 14:58:19 -0300

    Config da API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { filmeService } from './services/filme-service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'GerenciadorFilmes';
+export class AppComponent implements OnInit {
+  constructor(private filmeService: filmeService) {}
+
+  ngOnInit(): void {
+    this.filmeService.selecionarFilmesPopulares().subscribe((f) => {});
+  }
 }
